852d61e308ec646e2b8a6b74bb1fc26226f33592
iamdeepakram <deepaksutrakar@aol.com>
2021-06-27 02:07:31 +0530

    added tailwindcss config and restructure the project.

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'  //utilizing useState
+
+
+function   Navbar() {
+
+
+    return (
+        <>
+       <nav className="navbar">
+
+       </nav> 
+       
+        </>
+    )
+}
+
+export default Navbar;
