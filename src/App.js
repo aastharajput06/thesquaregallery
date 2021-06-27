31d5adf2176d5bffb3668f682ee95e89fd32c8a5
aastharajput06 <aastharajput967@gmail.com>
2021-06-28 02:37:27 +0530

    added all the buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import Navbar from './components/Navbar';
 import Landingpage from './components/Landingpage';
-
+import './index.css';
 
 
 // import { render } from '@testing-library/react';
 
 
 
-
-
-
+function clickMe(){
+  alert(" Thankyou for Registering!!");
+}
 
 
 
 function App() {
     return (
-      <div className="App">
-
+      <>
+        <div>
+           <button class="bg-green-300 hover:bg-green-200 text-white font-bold py-2 px-4 rounded">
+            Create Account/Log In
+             </button>
+          </div> 
+         <div>
+           <button class="bg-green-500 hover:bg-green-200 text-white font-bold py-2 px-4 rounded"
+            onClick= {clickMe}>
+             Submit
+             </button>
+          </div> 
+         <div>
+           <button class="bg-red-500 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">
+            Register
+             </button>
+          </div> 
+         <div>
+           <a href ="https://twitter.com/?lang=en" target ="_blank">
+           <button class="bg-blue-400 hover:bg-blue-200 text-white font-bold py-2 px-4 rounded">
+             Twitter
+             </button>
+            </a> 
+          </div> 
+         <div>
+         <a href = "https://www.facebook.com/" target = "_blank">
+           <button class="bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
+             facebook
+             </button>
+          </a>  
+          </div> 
+       
+       
         <Navbar />
+      
         <Landingpage />
 
-      </div>
-
+      </>
     );
   }
 
--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -6,8 +6,7 @@ class loading extends React.Component {
     render() {
         return (
             <div>
-
-            
+        
                 <div class="text-xl font-medium text-black">ChitChat</div>
                 <p class="text-gray-500">You have a new message!</p>
                
